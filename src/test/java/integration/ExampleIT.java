6621a2de3ff79f390047c0ab924b22eadb17e11d
Sergei Petrunin <halfstrik@gmail.com>
2013-07-07 13:53:09 +0400

    Migrated client only, get java.lang.NoSuchMethodError: javax.ws.rs.core.MultivaluedMap.addAll

--- src/test/java/integration/ExampleIT.java
+++ src/test/java/integration/ExampleIT.java
@@ -1,17 +1,16 @@
 package integration;
 
 import business.entity.ExampleEntity;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.json.JSONConfiguration;
 import launcher.Launcher;
 import org.eclipse.jetty.server.ServerConnector;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import static org.junit.Assert.assertEquals;
@@ -33,11 +32,9 @@ public class ExampleIT {
 
     @Test
     public void ExampleServiceExampleEndpoint_withServerRunning_expectedToReturnExampleEntity() {
-        ClientConfig clientConfig = new DefaultClientConfig();
-        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-        Client c = Client.create(clientConfig);
-        WebResource r = c.resource("http://localhost:8080").path("rest/exampleService/exampleEndpoint");
-        ExampleEntity actual = r.accept(MediaType.APPLICATION_JSON).get(ExampleEntity.class);
+        Client c = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        WebTarget t = c.target("http://localhost:8080").path("rest/exampleService/exampleEndpoint");
+        ExampleEntity actual = t.request(MediaType.APPLICATION_JSON).get(ExampleEntity.class);
         assertEquals("example",actual.exampleProperty);
     }
 }
